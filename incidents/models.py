835490e4511f397163d4518c42149f937d93907a
Mery Chonishvili <merychonishvili@gmail.com>
2022-10-24 12:54:18 +0400

    Fix many-to-many relationships in models

--- incidents/models.py
+++ incidents/models.py
@@ -14,10 +14,16 @@ class Incident(models.Model):
     name = models.CharField(max_length=128, choices=[(t.name, t.value) for t in IncidentType])
     description = models.TextField(blank=True)
     time_creation = models.DateTimeField(default=timezone.now)
+    cves = models.ManyToManyField("CommonVulnerabilityExposure", related_name="cve_incidents")
+    targets = models.ManyToManyField("IncidentTarget", related_name="target_incidents")
 
     @property
     def cve_numbers(self):
-        return [cve.cve_number for cve in self.cve_objects]
+        return [cve.cve_number for cve in self.cves.all()]
+
+    @property
+    def object(self):
+        return [t.name for t in self.targets.all()]
 
     class Meta:
         db_table = "incidents"
@@ -26,14 +32,14 @@ class Incident(models.Model):
 class IncidentTarget(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128)
-    incidents = models.ManyToManyField(Incident, related_name="objects", db_table="incident_targets")
+
+    class Meta:
+        db_table = "incident_targets"
 
 
 class CommonVulnerabilityExposure(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    cve_number = models.CharField(max_length=64)
-    cna_reference = models.CharField(max_length=256)
-    incidents = models.ManyToManyField(Incident, related_name="cve_objects", blank=True, db_table="incident_cves")
+    cve_number = models.CharField(max_length=64, unique=True)
 
     class Meta:
         db_table = "cves"
